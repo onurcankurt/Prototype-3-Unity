91877494b7e93cde2a42944cad5798b191f3a704
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-07 17:45:53 +0300

    jumping and moving added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody playerRb;
+
+    public float jumpForce;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false; 
+        }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
